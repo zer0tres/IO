985d25cdb92d64d561af990d583600103971bab8
zer0tres <109388900+zer0tres@users.noreply.github.com>
2025-09-26 16:05:15 +0000

    botão?

--- Fund. Bradsc./HTML-CSS-JS/site-simples/app.js
+++ Fund. Bradsc./HTML-CSS-JS/site-simples/app.js
@@ -1,1 +1,14 @@
 'use strict'
+const switcher = document.querySelector('.btn');
+switcher.addEventListener('click', function(){
+    document.body.classList.toggle('dark-theme')
+
+    var className = document.body.className;
+    if(className == "ligth-theme") {
+        this.textcontent = "Dark";
+    }
+    else {
+        this.textcontent = "Light";
+    }
+    console.log('current class name:'+ className);
+});
