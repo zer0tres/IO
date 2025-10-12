c0926e297b9e0ed6e7e8de40065369fd2042f63d
zer0tres <109388900+zer0tres@users.noreply.github.com>
2025-10-12 20:36:58 +0000

    adiciona exemplo de uso para a função de saudação personalizada

--- Fund. Bradsc./GITHUB_COPILOT/primeiro-copilot.js
+++ Fund. Bradsc./GITHUB_COPILOT/primeiro-copilot.js
@@ -2,3 +2,7 @@
 function saudacao(nome) {
     return `Olá, ${nome}! Seja bem-vindo(a)!`;
 }
+
+//exemplo de uso
+console.log(saudacao("Maria")); // Olá, Maria! Seja bem-vindo(a)!
+console.log(saudacao("João"));  // Olá, João! Seja bem-vindo(a)!
