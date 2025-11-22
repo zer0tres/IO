4cff321d97c13b2379b47bc2eeb829576a179c7e
zer0tres <109388900+zer0tres@users.noreply.github.com>
2025-11-22 17:36:24 +0000

    classificador e condições

---
+++ python/aula2_controle_fluxo.py
@@ -0,0 +1,15 @@
+# 15
+
+idade = int(input("Me diga sua idade: "))
+if idade <= 4 :
+    print("você é um nene")
+elif idade < 13:
+    print("você é uma criança")
+elif idade < 17:
+    print("você é adolescente")
+elif idade < 18:
+    print("você ainda é menor de idade")
+elif idade < 40:
+    print("você já é maior de idade,um adulto funcional")
+elif idade >40:
+    print("voce esta a um pulo de morrer")
