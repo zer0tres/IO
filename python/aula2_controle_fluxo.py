648ec1f65eed8ec366797bcd466be8677a79b354
zer0tres <109388900+zer0tres@users.noreply.github.com>
2026-01-15 20:56:23 +0000

    corrige condições de idade para inclusão de limites superiores nas mensagens

--- python/aula2_controle_fluxo.py
+++ python/aula2_controle_fluxo.py
@@ -3,13 +3,13 @@
 idade = int(input("Me diga sua idade: "))
 if idade <= 4 :
     print("você é um nene")
-elif idade < 13:
+elif idade <= 13:
     print("você é uma criança")
-elif idade < 17:
+elif idade <= 17:
     print("você é adolescente")
-elif idade < 18:
+elif idade <= 18:
     print("você ainda é menor de idade")
-elif idade < 40:
+elif idade <= 40:
     print("você já é maior de idade,um adulto funcional")
-elif idade >40:
+else:
     print("voce esta a um pulo de morrer")
